c22c8232097bbbcafa235394c38a98a960be3bff
auxten <pengcheng.wang@thunderdb.io>
2018-04-18 14:03:42 +0800

    add space to align

--- build.sh
+++ build.sh
@@ -14,10 +14,10 @@ version=`getversion`
 pkgpath="github.com/thunderdb/ThunderDB/cmd/thunderdbd"
 
 echo "build thunderdbd-linux"
-GOOS=linux GOARCH=amd64  go build -ldflags "-X main.versionStr=${version}"  -o bin/thunderdbd-linux ${pkgpath}
+GOOS=linux GOARCH=amd64   go build -ldflags "-X main.versionStr=${version}"  -o bin/thunderdbd-linux ${pkgpath}
 
 #echo "build thunderdbd-osx"
-GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.versionStr=${version}"  -o bin/thunderdbd-osx ${pkgpath}
+GOOS=darwin GOARCH=amd64  go build -ldflags "-X main.versionStr=${version}"  -o bin/thunderdbd-osx ${pkgpath}
 
 #echo "build thunderdbd-windows"
 #GOOS=windows GOARCH=386  go build -ldflags "-X main.versionStr=${version}"  -o bin/thunderdbd-win ${pkgpath}

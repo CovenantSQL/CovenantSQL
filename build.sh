0204c57d3842280e5ba89eacfcc20a9b3c044bc0
auxten <pengcheng.wang@thunderdb.io>
2018-04-26 01:16:11 +0800

    fix build.sh use default GOOS GOARCH

--- build.sh
+++ build.sh
@@ -14,11 +14,13 @@ cd `dirname $0`
 version=`getversion`
 pkgpath="github.com/thunderdb/ThunderDB/cmd/thunderdbd"
 
+go build -ldflags "-X main.version=${version}"  -o bin/thunderdbd ${pkgpath}
+
 #echo "build thunderdbd-linux"
 #GOOS=linux GOARCH=amd64   go build -ldflags "-X main.version=${version}"  -o bin/thunderdbd-linux ${pkgpath}
 
-echo "build thunderdbd-osx"
-GOOS=darwin GOARCH=amd64  go build -ldflags "-X main.version=${version}"  -o bin/thunderdbd-osx ${pkgpath}
+#echo "build thunderdbd-osx"
+#GOOS=darwin GOARCH=amd64  go build -ldflags "-X main.version=${version}"  -o bin/thunderdbd-osx ${pkgpath}
 
 #echo "build thunderdbd-windows"
 #GOOS=windows GOARCH=386  go build -ldflags "-X main.version=${version}"  -o bin/thunderdbd-win ${pkgpath}

c442bb876a54ce48c8de676f59536e2a7e5ded0b
lambda <zqguo92@gmail.com>
2018-05-29 14:58:53 +0800

    add more test case for protobuf

--- sqlchain/sqlchaintypes_test.go
+++ sqlchain/sqlchaintypes_test.go
@@ -91,4 +91,75 @@ func TestUtxoEntry(t *testing.T) {
 	if strings.Compare(S, utxoHeader.GetSignature().GetS()) != 0 {
 		t.Errorf("S should be %s, but get %s", S, utxoHeader.GetSignature().GetS())
 	}
+
+	_ = utxoEntry.String()
+	utxoEntry.ProtoMessage()
+	_, _ = utxoEntry.Descriptor()
+	b := make([]byte, utxoEntry.XXX_Size())
+	b, _ = utxoEntry.XXX_Marshal(b, true)
+	_ = utxoEntry.XXX_Unmarshal(b)
+	utxoEntry.XXX_DiscardUnknown()
+	utxoEntry.Reset()
+}
+
+func TestTxType(t *testing.T) {
+	tt := TxType_QUERY
+
+	_ = tt.String()
+	_, _ = tt.EnumDescriptor()
+}
+
+func TestSignature(t *testing.T) {
+	sig := Signature{
+		R: "122718002921",
+		S: "192890180857",
+	}
+
+	_ = sig.String()
+	sig.ProtoMessage()
+	_, _ = sig.Descriptor()
+	b := make([]byte, sig.XXX_Size())
+	b, _ = sig.XXX_Marshal(b, true)
+	_ = sig.XXX_Unmarshal(b)
+	sig.XXX_DiscardUnknown()
+	sig.Reset()
+}
+
+func TestPublicKey(t *testing.T) {
+	pk := PublicKey{
+		PublicKey: []byte{
+			0x04, 0x11, 0xdb, 0x93, 0xe1, 0xdc, 0xdb, 0x8a,
+			0x01, 0x6b, 0x49, 0x84, 0x0f, 0x8c, 0x53, 0xbc, 0x1e,
+			0xb6, 0x8a, 0x38, 0x2e, 0x97, 0xb1, 0x48, 0x2e, 0xca,
+			0xd7, 0xb1, 0x48, 0xa6, 0x90, 0x9a, 0x5c, 0xb2, 0xe0,
+			0xea, 0xdd, 0xfb, 0x84, 0xcc, 0xf9, 0x74, 0x44, 0x64,
+			0xf8, 0x2e, 0x16, 0x0b, 0xfa, 0x9b, 0x8b, 0x64, 0xf9,
+			0xd4, 0xc0, 0x3f, 0x99, 0x9b, 0x86, 0x43, 0xf6, 0x56,
+			0xb4, 0x12, 0xa3,
+		},
+	}
+
+	_ = pk.String()
+	pk.ProtoMessage()
+	_, _ = pk.Descriptor()
+	b := make([]byte, pk.XXX_Size())
+	b, _ = pk.XXX_Marshal(b, true)
+	_ = pk.XXX_Unmarshal(b)
+	pk.XXX_DiscardUnknown()
+	pk.Reset()
+}
+
+func TestHash(t *testing.T) {
+	h := Hash{
+		Hash: []byte{0x10, 0x38, 0xa1, 0x22},
+	}
+
+	_ = h.String()
+	h.ProtoMessage()
+	_, _ = h.Descriptor()
+	b := make([]byte, h.XXX_Size())
+	b, _ = h.XXX_Marshal(b, true)
+	_ = h.XXX_Unmarshal(b)
+	h.XXX_DiscardUnknown()
+	h.Reset()
 }

47b5004214e75b9023974cccd1ee5f22a2770e53
auxten <pengcheng.wang@thunderdb.io>
2018-06-08 12:07:41 +0800

    add crypto/etls/encrypt.go unittest

--- crypto/etls/encrypt.go
+++ crypto/etls/encrypt.go
@@ -29,7 +29,7 @@ import (
 // KeyDerivation .according to ANSI X9.63 we should do a key derivation before using
 // it as a symmetric key, there is not really a common standard KDF(Key Derivation Func).
 // But as SSL/TLS/DTLS did it described in "RFC 4492 TLS ECC", we prefer a Double
-// SHA3-256 with it.
+// SHA-256 with it.
 func KeyDerivation(rawKey []byte, keyLen int, hSuite *hash.HashSuite) (key []byte) {
 	hashLen := hSuite.HashLen
 
---
+++ crypto/etls/encrypt_test.go
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 The ThunderDB Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package etls
+
+import (
+	"testing"
+
+	"bytes"
+
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/thunderdb/ThunderDB/crypto/hash"
+)
+
+func TestKeyDerivation(t *testing.T) {
+	hSuite := &hash.HashSuite{
+		HashLen:  hash.HashBSize,
+		HashFunc: hash.DoubleHashB,
+	}
+
+	Convey("get addr", t, func() {
+		rawKey := bytes.Repeat([]byte("a"), 1000)
+		dKey := KeyDerivation(rawKey, 100, hSuite)
+		So(dKey, ShouldHaveLength, 100)
+	})
+}

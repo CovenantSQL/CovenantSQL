b2b4f48d42c95a54ae582e7688408d8e6c73a5fa
leventeliu <zhiteng.liu@thunderdb.io>
2018-06-11 17:37:34 +0800

    Update vendor

---
+++ vendor/github.com/gopherjs/gopherjs/compiler/version_check.go
@@ -0,0 +1,9 @@
+// +build !go1.11
+// +build go1.10
+
+package compiler
+
+const ___GOPHERJS_REQUIRES_GO_VERSION_1_10___ = true
+
+// Version is the GopherJS compiler version string.
+const Version = "1.10-4"

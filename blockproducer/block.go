b6a38c2d27de1bf9b4f824c2e0b007e1384f4153
zeqing-guo <zqguo92@gmail.com>
2018-06-13 00:03:56 +0800

    add bp block and its test case

--- blockproducer/block.go
+++ blockproducer/block.go
@@ -15,3 +15,282 @@
  */
 
 package blockproducer
+
+import (
+	"time"
+
+	"math/big"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/gogo/protobuf/proto"
+	"github.com/thunderdb/ThunderDB/crypto/hash"
+	"github.com/thunderdb/ThunderDB/types"
+)
+
+// Header is the header structure that will be signed
+type Header struct {
+	Version    int32
+	Producer   *types.AccountAddress
+	Root       hash.Hash
+	Parent     hash.Hash
+	MerkleRoot hash.Hash
+	Timestamp  time.Time
+}
+
+func (h *Header) marshal() ([]byte, error) {
+	return proto.Marshal(&types.BPHeader{
+		Version:    h.Version,
+		Producer:   h.Producer,
+		Root:       &types.Hash{Hash: h.Root[:]},
+		Parent:     &types.Hash{Hash: h.Parent[:]},
+		MerkleRoot: &types.Hash{Hash: h.MerkleRoot[:]},
+		Timestamp:  h.Timestamp.UnixNano(),
+	})
+}
+
+func (h *Header) toHeader(bpHeader types.BPHeader) error {
+	rootHash, err := hash.NewHash(bpHeader.Root.Hash)
+	if err != nil {
+		return err
+	}
+
+	parentHash, err := hash.NewHash(bpHeader.Parent.Hash)
+	if err != nil {
+		return err
+	}
+
+	MerkleRootHash, err := hash.NewHash(bpHeader.MerkleRoot.Hash)
+	if err != nil {
+		return err
+	}
+
+	h.Version = bpHeader.Version
+	h.Producer = bpHeader.Producer
+	h.Root = *rootHash
+	h.Parent = *parentHash
+	h.MerkleRoot = *MerkleRootHash
+	h.Timestamp = time.Unix(0, bpHeader.Timestamp).UTC()
+
+	return nil
+}
+
+// SignedHeader is the full header structure including
+// Header and signature information
+type SignedHeader struct {
+	Header
+	BlockHash hash.Hash
+	PublicKey *btcec.PublicKey
+	Signature *btcec.Signature
+}
+
+func (h *SignedHeader) toBPSignedHeader() *types.BPSignedHeader {
+	return &types.BPSignedHeader{
+		Header: &types.BPHeader{
+			Version:    h.Version,
+			Producer:   h.Producer,
+			Root:       &types.Hash{Hash: h.Root[:]},
+			Parent:     &types.Hash{Hash: h.Parent[:]},
+			MerkleRoot: &types.Hash{Hash: h.MerkleRoot[:]},
+			Timestamp:  h.Timestamp.UnixNano(),
+		},
+		BlockHash: &types.Hash{Hash: h.BlockHash[:]},
+		Signee: &types.PublicKey{
+			PublicKey: h.PublicKey.SerializeUncompressed(),
+		},
+		Signature: &types.Signature{
+			R: h.Signature.R.String(),
+			S: h.Signature.S.String(),
+		},
+	}
+}
+
+func (h *SignedHeader) toSignedHeader(bpSignedHeader types.BPSignedHeader) error {
+	blockHash, err := hash.NewHash(bpSignedHeader.BlockHash.Hash)
+	if err != nil {
+		return err
+	}
+
+	publicKey, err := btcec.ParsePubKey(bpSignedHeader.Signee.PublicKey, btcec.S256())
+	if err != nil {
+		return err
+	}
+
+	r := new(big.Int)
+	r.SetString(bpSignedHeader.Signature.R, 10)
+	s := new(big.Int)
+	s.SetString(bpSignedHeader.Signature.S, 10)
+
+	h.toHeader(*bpSignedHeader.Header)
+	h.BlockHash = *blockHash
+	h.PublicKey = publicKey
+	h.Signature = &btcec.Signature{
+		R: r,
+		S: s,
+	}
+	return nil
+}
+
+func (h *SignedHeader) marshal() ([]byte, error) {
+	return proto.Marshal(h.toBPSignedHeader())
+}
+
+func (h *SignedHeader) unmarshal(buff []byte) error {
+	pbBPSignedHeader := types.BPSignedHeader{}
+	err := proto.Unmarshal(buff, &pbBPSignedHeader)
+	if err != nil {
+		return err
+	}
+
+	err = h.toSignedHeader(pbBPSignedHeader)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TxData is the details of a tx
+type TxData struct {
+	AccountNonce uint64
+	Recipient    *types.AccountAddress
+	Amount       *big.Int
+	Payload      []byte
+
+	Signature *btcec.Signature
+	PublicKey *btcec.PublicKey
+}
+
+func (t *TxData) toBPTxData() *types.BPTxData {
+	return &types.BPTxData{
+		AccountNonce: t.AccountNonce,
+		Recipient:    t.Recipient,
+		Amount:       t.Amount.Bytes(),
+		Payload:      t.Payload,
+		Signature: &types.Signature{
+			R: t.Signature.R.String(),
+			S: t.Signature.S.String(),
+		},
+		Signee: &types.PublicKey{
+			PublicKey: t.PublicKey.SerializeUncompressed(),
+		},
+	}
+}
+
+func (t *TxData) marshal() ([]byte, error) {
+	return proto.Marshal(t.toBPTxData())
+}
+
+func (t *TxData) toTxData(bpTxData types.BPTxData) error {
+	publicKey, err := btcec.ParsePubKey(bpTxData.Signee.PublicKey, btcec.S256())
+	if err != nil {
+		return err
+	}
+
+	amount := new(big.Int)
+	amount.SetBytes(bpTxData.Amount)
+
+	r := new(big.Int)
+	r.SetString(bpTxData.Signature.R, 10)
+	s := new(big.Int)
+	s.SetString(bpTxData.Signature.S, 10)
+
+	t.AccountNonce = bpTxData.AccountNonce
+	t.Recipient = bpTxData.Recipient
+	t.Amount = amount
+	t.Payload = bpTxData.Payload
+	t.Signature = &btcec.Signature{
+		R: r,
+		S: s,
+	}
+	t.PublicKey = publicKey
+
+	return nil
+}
+
+// Tx includes TxData and TxData's hash
+type Tx struct {
+	TxHash hash.Hash
+	TxData TxData
+}
+
+func (t *Tx) toTx(BPTx types.BPTx) error {
+	txHash, err := hash.NewHash(BPTx.TxHash.Hash)
+	if err != nil {
+		return err
+	}
+
+	err = t.TxData.toTxData(*BPTx.TxData)
+	if err != nil {
+		return err
+	}
+
+	t.TxHash = *txHash
+
+	return nil
+}
+
+func (t *Tx) toBPTx() *types.BPTx {
+	return &types.BPTx{
+		// lambda: warning!!!! it will not pass the test case if i use t.TxHash[:] and i do not know why
+		TxHash: &types.Hash{Hash: t.TxHash.CloneBytes()},
+		TxData: t.TxData.toBPTxData(),
+	}
+}
+
+func (t *Tx) marshal() ([]byte, error) {
+	return proto.Marshal(t.toBPTx())
+}
+
+func (t *Tx) unmarshal(buff []byte) error {
+	bpTx := &types.BPTx{}
+	err := proto.Unmarshal(buff, bpTx)
+	if err != nil {
+		return err
+	}
+
+	err = t.toTx(*bpTx)
+	return err
+}
+
+// Block is generated by Block Producer
+type Block struct {
+	Header *SignedHeader
+	Tx     []Tx
+}
+
+func (b *Block) marshal() ([]byte, error) {
+	bpTx := make([]*types.BPTx, len(b.Tx))
+
+	for i, tx := range b.Tx {
+		bpTx[i] = tx.toBPTx()
+	}
+
+	return proto.Marshal(&types.BPBlock{
+		Header: b.Header.toBPSignedHeader(),
+		Tx:     bpTx[:],
+	})
+}
+
+func (b *Block) unmarshal(buff []byte) error {
+	block := &types.BPBlock{}
+	err := proto.Unmarshal(buff, block)
+	if err != nil {
+		return err
+	}
+
+	header := SignedHeader{}
+	err = header.toSignedHeader(*block.Header)
+	if err != nil {
+		return err
+	}
+
+	b.Header = &header
+
+	txes := make([]Tx, len(block.Tx))
+	for i, bpTx := range block.Tx {
+		txes[i].toTx(*bpTx)
+	}
+
+	b.Tx = txes
+	return nil
+}

1bf0a2ed0b3430e32c9b11eab348648022106a66
auxten <pengcheng.wang@thunderdb.io>
2018-06-20 12:49:22 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -13,14 +13,14 @@ cd `dirname $0`
 
 version=`getversion`
 
-idminer_pkgpath="github.com/thunderdb/ThunderDB/cmd/idminer"
+idminer_pkgpath="gitlab.com/thunderdb/ThunderDB/cmd/idminer"
 go build -ldflags "-X main.version=${version}"  -o bin/idminer ${idminer_pkgpath}
 
-thunderdbd_pkgpath="github.com/thunderdb/ThunderDB/cmd/thunderdbd"
-go build -ldflags "-X main.version=${version} -X github.com/thunderdb/ThunderDB/conf.Role=B"  -o bin/thunderdbd ${thunderdbd_pkgpath}
+thunderdbd_pkgpath="gitlab.com/thunderdb/ThunderDB/cmd/thunderdbd"
+go build -ldflags "-X main.version=${version} -X gitlab.com/thunderdb/ThunderDB/conf.Role=B"  -o bin/thunderdbd ${thunderdbd_pkgpath}
 
-miner_pkgpath="github.com/thunderdb/ThunderDB/cmd/miner"
-go build -ldflags "-X main.version=${version} -X github.com/thunderdb/ThunderDB/conf.Role=M"  -o bin/thunderminerd ${miner_pkgpath}
+miner_pkgpath="gitlab.com/thunderdb/ThunderDB/cmd/miner"
+go build -ldflags "-X main.version=${version} -X gitlab.com/thunderdb/ThunderDB/conf.Role=M"  -o bin/thunderminerd ${miner_pkgpath}
 
 #echo "build thunderdbd-linux"
 #GOOS=linux GOARCH=amd64   go build -ldflags "-X main.version=${version}"  -o bin/thunderdbd-linux ${pkgpath}

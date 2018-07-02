6ef7662bd07e9df347f17069f82b87356b272d6c
Qi Xiao <qi.xiao@thunderdb.io>
2018-07-02 17:05:18 +0800

    Add dbms config/meta

--- worker/dbms.go
+++ worker/dbms.go
@@ -16,12 +16,117 @@
 
 package worker
 
-const (
-	// DBKayakRPCName defines rpc service name of database internal consensus.
-	DBKayakRPCName = "DatabaseConsensus"
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
 
-	// DBServiceRPCName defines rpc service name of database external query api.
-	DBServiceRPCName = "DatabaseService"
+	"gitlab.com/thunderdb/ThunderDB/utils"
 )
 
-// TODO(xq262144) define multi-database management logic
+const (
+	// DBKayakRPCName defines rpc service name of database internal consensus.
+	DBKayakRPCName = "DBC" // aka. database consensus
+
+	// DBServiceRPCName defines rpc service name of database external query api.
+	DBServiceRPCName = "DBS" // aka. database service
+
+	// DBMetaFileName defines dbms meta file name.
+	DBMetaFileName = "db.meta"
+)
+
+// DBMS defines a database management instance.
+type DBMS struct {
+	cfg      *DBMSConfig
+	metaLock sync.Mutex
+	meta     *DBMSMeta
+}
+
+// NewDBMS returns new database management instance.
+func NewDBMS(cfg *DBMSConfig) (dbms *DBMS, err error) {
+	dbms = &DBMS{
+		cfg: cfg,
+	}
+
+	return
+}
+
+func (dbms *DBMS) readMeta() (err error) {
+	dbms.metaLock.Lock()
+	defer dbms.metaLock.Unlock()
+
+	filePath := filepath.Join(dbms.cfg.RootDir, DBMetaFileName)
+	dbms.meta = new(DBMSMeta)
+
+	var fileContent []byte
+
+	if fileContent, err = ioutil.ReadFile(filePath); err != nil {
+		// if not exists
+		if err == os.ErrNotExist {
+			// new without meta
+			err = nil
+			return
+		}
+
+		return
+	}
+
+	err = utils.DecodeMsgPack(fileContent, dbms.meta)
+
+	return
+}
+
+func (dbms *DBMS) writeMeta() (err error) {
+	dbms.metaLock.Lock()
+	defer dbms.metaLock.Unlock()
+
+	if dbms.meta == nil {
+		dbms.meta = new(DBMSMeta)
+	}
+
+	var buf *bytes.Buffer
+	if buf, err = utils.EncodeMsgPack(dbms.meta); err != nil {
+		return
+	}
+
+	filePath := filepath.Join(dbms.cfg.RootDir, DBMetaFileName)
+	err = ioutil.WriteFile(filePath, buf.Bytes(), 0644)
+
+	return
+}
+
+// Init defines dbms init logic.
+func (dbms *DBMS) Init() (err error) {
+	// read meta
+	err = dbms.readMeta()
+
+	return
+}
+
+func (dbms *DBMS) create() (err error) {
+	// called by rpc to call create database logic
+	return
+}
+
+func (dbms *DBMS) drop() (err error) {
+	// called by rpc to call drop database logic
+	return
+}
+
+func (dbms *DBMS) update() (err error) {
+	// called by rpc to call drop database logic
+	return
+}
+
+// Shutdown defines dbms shutdown logic.
+func (dbms *DBMS) Shutdown() (err error) {
+	// shutdown databases
+	// TODO(xq262144), shutdown databases
+
+	// persist meta
+	err = dbms.writeMeta()
+
+	return
+}

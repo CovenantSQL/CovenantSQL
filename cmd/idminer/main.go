e2e50f9b760f8cc3db2eea5dcb6b2596c83ae407
Qi Xiao <qi.xiao@thunderdb.io>
2018-07-17 18:24:06 +0800

    Update idminer to support key generation and key reader

--- cmd/idminer/main.go
+++ cmd/idminer/main.go
@@ -18,42 +18,89 @@ package main
 
 import (
 	"encoding/hex"
+	"flag"
+	"math"
+	"math/rand"
 	"os"
 	"os/signal"
-	"syscall"
-
 	"runtime"
-
-	"math"
-
-	"math/rand"
+	"syscall"
 	"time"
 
 	"gitlab.com/thunderdb/ThunderDB/crypto/asymmetric"
+	"gitlab.com/thunderdb/ThunderDB/crypto/kms"
 	mine "gitlab.com/thunderdb/ThunderDB/pow/cpuminer"
 	"gitlab.com/thunderdb/ThunderDB/utils/log"
 )
 
 var (
-	version = "unknown"
+	version        = "unknown"
+	tool           string
+	publicKeyHex   string
+	privateKeyFile string
+	difficulty     int
 )
 
+func init() {
+	flag.StringVar(&tool, "tool", "miner", "tool type, miner, keygen, keytool")
+	flag.StringVar(&publicKeyHex, "public", "", "public key hex string to mine node id/nonce")
+	flag.StringVar(&privateKeyFile, "private", "", "private key file to generate/show")
+	flag.IntVar(&difficulty, "difficulty", 256, "difficulty for miner to mine nodes")
+}
+
 func main() {
-	// TODO(auxten): keypair and nodeID generator
 	log.Infof("idminer build: %s", version)
-	if len(os.Args) != 2 {
-		log.Error("usage: ./idminer publicKeyHex")
+	flag.Parse()
+
+	switch tool {
+	case "miner":
+		if publicKeyHex == "" && privateKeyFile == "" {
+			// error
+			log.Error("publicKey or privateKey is required in miner mode")
+			os.Exit(1)
+		}
+		runMiner()
+	case "keygen":
+		if privateKeyFile == "" {
+			// error
+			log.Error("privateKey path is required for keygen")
+			os.Exit(1)
+		}
+		runKeygen()
+	case "keytool":
+		if privateKeyFile == "" {
+			// error
+			log.Error("privateKey path is required for keytool")
+			os.Exit(1)
+		}
+		runKeytool()
+	default:
+		flag.Usage()
 		os.Exit(1)
 	}
-	publicKeyString := os.Args[1]
-	publicKeyBytes, err := hex.DecodeString(publicKeyString)
-	if err != nil {
-		log.Fatalf("error converting hex: %s", err)
+
+}
+
+func runMiner() {
+	var publicKey *asymmetric.PublicKey
+
+	if publicKeyHex != "" {
+		publicKeyBytes, err := hex.DecodeString(publicKeyHex)
+		if err != nil {
+			log.Fatalf("error converting hex: %s", err)
+		}
+		publicKey, err = asymmetric.ParsePubKey(publicKeyBytes)
+		if err != nil {
+			log.Fatalf("error converting public key: %s", err)
+		}
+	} else if privateKeyFile != "" {
+		privateKey, err := kms.LoadPrivateKey(privateKeyFile, []byte(""))
+		if err != nil {
+			log.Fatalf("load private key file faile: %v", err)
+		}
+		publicKey = privateKey.PubKey()
 	}
-	publicKey, err := asymmetric.ParsePubKey(publicKeyBytes)
-	if err != nil {
-		log.Fatalf("error converting public key: %s", err)
-	}
+
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(
 		signalCh,
@@ -83,14 +130,14 @@ func main() {
 			}
 			start := mine.Uint256{0, 0, 0, step*uint64(i) + uint64(rand.Uint32())}
 			log.Infof("miner #%d start: %v", i, start)
-			miner.ComputeBlockNonce(block, start, 256)
+			miner.ComputeBlockNonce(block, start, difficulty)
 		}(i)
 	}
 
 	sig := <-signalCh
 	log.Infof("received signal %s", sig)
 	for i := 0; i < cpuCount; i++ {
-		stopChs[i] <- struct{}{}
+		close(stopChs[i])
 	}
 
 	max := mine.NonceInfo{}
@@ -101,4 +148,26 @@ func main() {
 		}
 	}
 	log.Infof("nonce: %v", max)
+}
+
+func runKeygen() {
+	privateKey, _, err := asymmetric.GenSecp256k1KeyPair()
+	if err != nil {
+		log.Fatalf("generate key pair failed: %v", err)
+	}
+
+	if err = kms.SavePrivateKey(privateKeyFile, privateKey, []byte("")); err != nil {
+		log.Fatalf("save generated keypair failed: %v", err)
+	}
+
+	log.Infof("pubkey hex is: %s", hex.EncodeToString(privateKey.PubKey().Serialize()))
+}
+
+func runKeytool() {
+	privateKey, err := kms.LoadPrivateKey(privateKeyFile, []byte(""))
+	if err != nil {
+		log.Fatalf("load private key failed: %v", err)
+	}
+
+	log.Infof("pubkey hex is: %s", hex.EncodeToString(privateKey.PubKey().Serialize()))
 }

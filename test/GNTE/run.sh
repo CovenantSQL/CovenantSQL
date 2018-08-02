d2cbb6c319c54019eadb6a618b0f0dd803ca870f
auxten <pengcheng.wang@thunderdb.io>
2018-08-02 21:14:14 +0800

    fix test/GNTE/run.sh

--- test/GNTE/run.sh
+++ test/GNTE/run.sh
@@ -5,6 +5,10 @@ PROJECT_DIR=$(cd $TEST_WD/../../; pwd)
 echo $PROJECT_DIR
 
 cd $PROJECT_DIR && ./build.sh && \
-    cp -r ./bin $TEST_WD/GNTE/scripts && \
-    cp -r ./conf/* $TEST_WD/GNTE/scripts
+    cp -r ./bin $TEST_WD/GNTE/scripts
+
+cd $TEST_WD && cp -r ./conf/* ./GNTE/scripts
+
+cd $TEST_WD/GNTE && bash -x ./build.sh
+cd $TEST_WD/GNTE && bash -x ./generate.sh ./scripts/gnte.yaml
 

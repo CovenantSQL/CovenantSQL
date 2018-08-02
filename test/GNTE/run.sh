bfced17226c418888d76040c13eb7752fa3025ab
auxten <pengcheng.wang@thunderdb.io>
2018-08-02 20:44:11 +0800

    add gnte test, tobe tested on linux

--- test/GNTE/run.sh
+++ test/GNTE/run.sh
@@ -4,5 +4,7 @@ TEST_WD=$(cd $(dirname $0)/; pwd)
 PROJECT_DIR=$(cd $TEST_WD/../../; pwd)
 echo $PROJECT_DIR
 
-cd $PROJECT_DIR && ./build.sh && cp -r ./bin $TEST_WD/GNTE/scripts
+cd $PROJECT_DIR && ./build.sh && \
+    cp -r ./bin $TEST_WD/GNTE/scripts && \
+    cp -r ./conf/* $TEST_WD/GNTE/scripts
 

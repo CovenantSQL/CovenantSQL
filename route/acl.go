826167d4c79d6713789edb43f1ac482643813521
Qi Xiao <qi.xiao@covenantsql.io>
2018-08-19 12:38:33 +0800

    Support fetch original request using rpc

--- route/acl.go
+++ route/acl.go
@@ -77,6 +77,8 @@ const (
 	DBSAck
 	// DBSDeploy is used by BP to create/drop/update database
 	DBSDeploy
+	// DBSGetRequest is used by observer to view original request
+	DBSGetRequest
 	// DBCCall is used by Miner for data consistency
 	DBCCall
 	// BPDBCreateDatabase is used by client to create database
@@ -120,6 +122,8 @@ func (s RemoteFunc) String() string {
 		return "DBS.Ack"
 	case DBSDeploy:
 		return "DBS.Deploy"
+	case DBSGetRequest:
+		return "DBS.GetRequest"
 	case DBCCall:
 		return "DBC.Call"
 	case BPDBCreateDatabase:

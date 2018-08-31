502cdb11129038719091f0dc5f99f251c87f2d8e
Qi Xiao <qi.xiao@covenantsql.io>
2018-08-31 15:52:37 +0800

    Add sqlite extension to cli

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -31,6 +31,7 @@ import (
 
 	"github.com/CovenantSQL/CovenantSQL/client"
 	"github.com/CovenantSQL/CovenantSQL/utils/log"
+	"github.com/CovenantSQL/go-sqlite3-encrypt"
 	"github.com/xo/dburl"
 	"github.com/xo/usql/drivers"
 	"github.com/xo/usql/env"
@@ -82,6 +83,46 @@ func (v *varsFlag) Set(value string) error {
 func init() {
 	// set command name of usql
 	text.CommandName = "covenantsql"
+
+	// register SQLite3 database
+	drivers.Register("sqlite3", drivers.Driver{
+		AllowMultilineComments: true,
+		ForceParams: drivers.ForceQueryParameters([]string{
+			"loc", "auto",
+		}),
+		Version: func(db drivers.DB) (string, error) {
+			var ver string
+			err := db.QueryRow(`SELECT sqlite_version()`).Scan(&ver)
+			if err != nil {
+				return "", err
+			}
+			return "SQLite3 " + ver, nil
+		},
+		Err: func(err error) (string, string) {
+			if e, ok := err.(sqlite3.Error); ok {
+				return strconv.Itoa(int(e.Code)), e.Error()
+			}
+
+			code, msg := "", err.Error()
+			if e, ok := err.(sqlite3.ErrNo); ok {
+				code = strconv.Itoa(int(e))
+			}
+
+			return code, msg
+		},
+		ConvertBytes: func(buf []byte, tfmt string) (string, error) {
+			// attempt to convert buf if it matches a time format, and if it
+			// does, then return a formatted time string.
+			s := string(buf)
+			if s != "" && strings.TrimSpace(s) != "" {
+				t := new(xoutil.SqTime)
+				if err := t.Scan(buf); err == nil {
+					return t.Format(tfmt), nil
+				}
+			}
+			return s, nil
+		},
+	})
 
 	// register CovenantSQL database
 	drivers.Register("covenantsql", drivers.Driver{

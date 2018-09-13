81f9c62deb036585bce1e5bf03d102498282085d
Qi Xiao <qi.xiao@covenantsql.io>
2018-09-13 17:42:48 +0800

    Add skeleton for verifier

--- cmd/faucet/verifier.go
+++ cmd/faucet/verifier.go
@@ -16,6 +16,36 @@
 
 package main
 
+import (
+	"github.com/PuerkitoBio/goquery"
+	"strings"
+)
+
+const (
+	uaPC       = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.9 Safari/537.36"
+	uaMobile   = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+	retryCount = 10
+)
+
 // Verifier defines the social media post content verifier.
 type Verifier struct {
+}
+
+func verifyFacebook(mediaURL string) (err error) {
+	var resp string
+	resp, err = makeRequest(mediaURL, uaPC, retryCount)
+	var doc *goquery.Document
+	doc, err = goquery.NewDocumentFromReader(strings.NewReader(resp))
+}
+
+func verifyTwitter(mediaURL string) (err error) {
+
+}
+
+func verifyWeibo(mediaURL string) (err error) {
+
+}
+
+func makeRequest(reqURL string, ua string, retry int) (response string, err error) {
+
 }

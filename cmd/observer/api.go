a7bac00b9204c7463655db25d27e6dba9b5c221d
auxten <pengcheng.wang@thunderdb.io>
2018-09-27 17:53:10 +0800

    Fix observer dynamic subscribe from oldest

--- cmd/observer/api.go
+++ cmd/observer/api.go
@@ -252,7 +252,7 @@ func (a *explorerAPI) getHighestBlock(rw http.ResponseWriter, r *http.Request) {
 	height, block, err := a.service.getHighestBlock(dbID)
 	if err == ErrNotFound {
 		// try to add subscription
-		err = a.service.subscribe(dbID, "")
+		err = a.service.subscribe(dbID, "oldest")
 		if err == nil {
 			height, block, err = a.service.getHighestBlock(dbID)
 			if err != nil {

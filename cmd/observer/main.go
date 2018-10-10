b888bb57354edd85f5e53d049f112497ba15ddf3
auxten <pengcheng.wang@thunderdb.io>
2018-10-10 11:17:22 +0800

    Fix missing -database args

--- cmd/observer/main.go
+++ cmd/observer/main.go
@@ -49,6 +49,7 @@ var (
 
 func init() {
 	flag.StringVar(&configFile, "config", "./config.yaml", "Config file path")
+	flag.StringVar(&dbID, "database", "", "database to listen for observation")
 	flag.BoolVar(&asymmetric.BypassSignature, "bypassSignature", false,
 		"Disable signature sign and verify, for testing")
 	flag.StringVar(&resetPosition, "reset", "", "reset subscribe position")

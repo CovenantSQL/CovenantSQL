6edc4c211e8b1ddff3475ef73ddcc2c8e3274be2
auxten <pengcheng.wang@thunderdb.io>
2018-10-15 10:53:07 +0800

    Add tree svg graph for pprof

--- cmd/miner/pprof.sh
+++ cmd/miner/pprof.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 go test -bench=^BenchmarkMinerTwo$ -benchtime=60s -run ^$
-go tool pprof -text miner1.profile
+go tool pprof -text miner1.profile > pprof.txt
+go tool pprof -svg miner1.profile > tree.svg
 go-torch --width=2400 miner1.profile

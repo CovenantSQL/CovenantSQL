772b1b4c39f2186a2f1825a6fdb153c1110aeb52
auxten <pengcheng.wang@thunderdb.io>
2018-10-26 16:10:41 +0800

    Lower BenchmarkMinerTwo benchtime=15s

--- cmd/cql-minerd/pprof.sh
+++ cmd/cql-minerd/pprof.sh
@@ -3,7 +3,7 @@
 ../../cleanupDB.sh
 ../../build.sh
 
-go test -bench=^BenchmarkMinerTwo$ -benchtime=60s -run ^$
+go test -bench=^BenchmarkMinerTwo$ -benchtime=15s -run ^$
 go tool pprof -text miner1.profile > pprof.txt
 go tool pprof -svg miner1.profile > tree.svg
 go-torch -t 180 --width=2400 miner1.profile

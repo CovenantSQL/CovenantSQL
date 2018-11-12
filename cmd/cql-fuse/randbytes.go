4ab23eae766230e2fb81606763a6f3507eaf86b6
auxten <pengcheng.wang@thunderdb.io>
2018-11-12 17:56:49 +0800

    Add missing randbytes.go

---
+++ cmd/cql-fuse/randbytes.go
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 The CovenantSQL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Copyright 2015 The Cockroach Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License. See the AUTHORS file
+// for names of contributors.
+//
+// Author: Marc Berhault (marc@cockroachlabs.com)
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+var randLetters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// NewPseudoRand returns an instance of math/rand.Rand seeded from crypto/rand
+// and its seed so we can easily and cheaply generate unique streams of
+// numbers. The created object is not safe for concurrent access.
+func NewPseudoRand() (*rand.Rand, int64) {
+	seed := time.Now().UnixNano()
+	return rand.New(rand.NewSource(seed)), seed
+}
+
+// RandBytes returns a byte slice of the given length with random
+// data.
+func RandBytes(r *rand.Rand, size int) []byte {
+	if size <= 0 {
+		return nil
+	}
+
+	arr := make([]byte, size)
+	for i := 0; i < len(arr); i++ {
+		arr[i] = randLetters[r.Intn(len(randLetters))]
+	}
+	return arr
+}

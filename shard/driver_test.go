42c0e9a969c27e435296b4d5f07c5948e59c2b07
auxten <pengcheng.wang@thunderdb.io>
2018-11-28 17:22:51 +0800

    Add complex select for test

--- shard/driver_test.go
+++ shard/driver_test.go
@@ -64,10 +64,11 @@ func TestSQLite3Driver(t *testing.T) {
 }
 
 func executeSQL(isSharding bool, db *sql.DB) (err error) {
-	var sqlStmt = fmt.Sprintf(`
+	const tableSchemaTpl = `
 	create table if not exists foo%s (id integer not null primary key, name text, time timestamp );
 	create index if not exists fooindex%s on foo%s ( time );
-	`, ShardSchemaToken, ShardSchemaToken, ShardSchemaToken)
+	`
+	var sqlStmt = fmt.Sprintf(tableSchemaTpl, ShardSchemaToken, ShardSchemaToken, ShardSchemaToken)
 	if isSharding {
 		sqlStmt = fmt.Sprintf("SHARDCONFIG foo time %d %d ",
 			864000, 1536000000) + sqlStmt
@@ -92,7 +93,7 @@ func executeSQL(isSharding bool, db *sql.DB) (err error) {
 	}
 
 	_, err = db.Exec(`insert into foo(id, name, time) values(?, :vv1, :vv2);`,
-		9, sql.Named("vv1", "sss"), sql.Named("vv2", 1536699999.11))
+		9, sql.Named("vv1", "sss"), sql.Named("vv2", 1536699988.11))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -120,9 +121,42 @@ func executeSQL(isSharding bool, db *sql.DB) (err error) {
 			log.Fatal(err)
 		}
 	}
-	//tx.Commit()
 
-	rows, err := db.Query("select id, name, time from foo")
+	if !isSharding {
+		_, err = db.Exec(fmt.Sprintf(tableSchemaTpl, "_ts_0000000000", "_ts_0000000000", "_ts_0000000000"))
+		if err != nil {
+			log.Printf("%q: %s\n", err, sqlStmt)
+			return
+		}
+		_, err = db.Exec(`insert into foo_ts_0000000000(id, name, time) values(61, 'sss', '2018-09-11');`)
+		if err != nil {
+			log.Printf("%q: %s\n", err, sqlStmt)
+			return
+		}
+	}
+	rows, err := db.Query(`select id, id, name, time from foo_ts_0000000000 limit 1;`+
+		"select count(1), max(id), id, foo_ts_0000000000.name, time from foo_ts_0000000000 where id < ? and name = :ll limit 10;",
+		100, sql.Named("ll", "sss"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var cnt, sum, id interface{}
+		var name string
+		var time interface{}
+		err = rows.Scan(&cnt, &sum, &id, &name, &time)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(cnt, sum, id, name, time)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err = db.Query("select id, name, time from foo")
 	if err != nil {
 		log.Fatal(err)
 	}

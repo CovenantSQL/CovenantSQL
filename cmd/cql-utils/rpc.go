2230da52e3cddd2fbe4d7d752c9e258fe1f92667
Qi Xiao <qi.xiao@covenantsql.io>
2018-12-26 17:12:33 +0800

    Support nonce inject in AddTx method

--- cmd/cql-utils/rpc.go
+++ cmd/cql-utils/rpc.go
@@ -19,14 +19,13 @@ package main
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
-	"os"
 	"reflect"
 	"strings"
 
 	bp "github.com/CovenantSQL/CovenantSQL/blockproducer"
 	pi "github.com/CovenantSQL/CovenantSQL/blockproducer/interfaces"
 	"github.com/CovenantSQL/CovenantSQL/client"
+	"github.com/CovenantSQL/CovenantSQL/crypto"
 	"github.com/CovenantSQL/CovenantSQL/crypto/asymmetric"
 	"github.com/CovenantSQL/CovenantSQL/crypto/kms"
 	"github.com/CovenantSQL/CovenantSQL/proto"
@@ -65,18 +64,17 @@ func init() {
 func runRPC() {
 	if configFile == "" {
 		// error
-		log.Error("config file path is required for rpc tool")
-		os.Exit(1)
+		log.Fatal("config file path is required for rpc tool")
+		return
 	}
 	if rpcEndpoint == "" || rpcName == "" || rpcReq == "" {
 		// error
-		log.Error("rpc payload is required for rpc tool")
-		os.Exit(1)
+		log.Fatal("rpc payload is required for rpc tool")
+		return
 	}
 
 	if err := client.Init(configFile, []byte("")); err != nil {
-		fmt.Printf("init rpc client failed: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("init rpc client failed: %v\n", err)
 		return
 	}
 
@@ -91,29 +89,51 @@ func runRPC() {
 
 	// fill the req with request body
 	if err := json.Unmarshal([]byte(rpcReq), req); err != nil {
-		fmt.Printf("decode request body failed: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("decode request body failed: %v\n", err)
 		return
+	}
+
+	// fill nonce if this is a AddTx request
+	if rpcName == route.MCCAddTx.String() {
+		if addTxReqType, ok := req.(*types.AddTxReq); ok {
+			var tx = addTxReqType.Tx
+			for {
+				if txWrapper, ok := tx.(*pi.TransactionWrapper); ok {
+					tx = txWrapper.Unwrap()
+				} else {
+					break
+				}
+			}
+
+			// unwrapped tx, find account nonce field and set
+			if err := fillTxNonce(tx); err != nil {
+				log.Fatalf("fill block producer transaction nonce failed: %v\n", err)
+				return
+			}
+
+			if err := checkAndSign(tx); err != nil {
+				log.Fatalf("sign transaction failed: %v\n", err)
+				return
+			}
+		}
 	}
 
 	// requires signature?
 	if err := checkAndSign(req); err != nil {
-		fmt.Printf("sign request failed: %v\n", err)
-		os.Exit(1)
+		log.Fatalf("sign request failed: %v\n", err)
 		return
 	}
 
-	fmt.Println("sending request")
+	log.Info("sending request")
 	spew.Dump(req)
 	if err := rpc.NewCaller().CallNode(proto.NodeID(rpcEndpoint), rpcName, req, resp); err != nil {
 		// send request failed
-		fmt.Printf("call rpc failed: %v\n", err)
-		os.Exit(1)
+		log.Infof("call rpc failed: %v\n", err)
 		return
 	}
 
 	// print the response
-	fmt.Println("got response")
+	log.Info("got response")
 	spew.Dump(resp)
 }
 
@@ -123,6 +143,8 @@ func checkAndSign(req interface{}) (err error) {
 	}
 
 	if canSignObj, ok := req.(canSign); ok {
+		log.Info("signing request")
+
 		var privKey *asymmetric.PrivateKey
 		if privKey, err = kms.GetLocalPrivateKey(); err != nil {
 			return
@@ -135,13 +157,47 @@ func checkAndSign(req interface{}) (err error) {
 	return
 }
 
+func fillTxNonce(tx pi.Transaction) (err error) {
+	rv := reflect.ValueOf(tx)
+	for i := 0; i != rv.NumField(); i++ {
+		if _, ok := rv.Field(i).Interface().(pi.AccountNonce); ok {
+			// nonce type
+			// generate nonce for account
+			var (
+				pubKey      *asymmetric.PublicKey
+				accountAddr proto.AccountAddress
+			)
+			if pubKey, err = kms.GetLocalPublicKey(); err != nil {
+				return
+			}
+			if accountAddr, err = crypto.PubKeyHash(pubKey); err != nil {
+				return
+			}
+			nonceReq := &types.NextAccountNonceReq{
+				Addr: accountAddr,
+			}
+			nonceResp := &types.NextAccountNonceResp{}
+			if err = rpc.NewCaller().CallNode(proto.NodeID(rpcEndpoint),
+				route.MCCNextAccountNonce.String(), nonceReq, nonceResp); err != nil {
+				return
+			}
+
+			rv.Field(i).SetUint(uint64(nonceResp.Nonce))
+			log.Infof("filled tx type %s nonce field %s with nonce %d",
+				tx.GetTransactionType().String(), rv.Type().Field(i).Name, nonceResp.Nonce)
+			break
+		}
+	}
+
+	return
+}
+
 func resolveRPCEntities() (req interface{}, resp interface{}) {
 	rpcParts := strings.SplitN(rpcName, ".", 2)
 
 	if len(rpcParts) != 2 {
 		// error rpc name
-		fmt.Printf("%v is not a valid rpc name\n", rpcName)
-		os.Exit(1)
+		log.Fatalf("%v is not a valid rpc name\n", rpcName)
 		return
 	}
 
@@ -158,8 +214,7 @@ func resolveRPCEntities() (req interface{}, resp interface{}) {
 			if method.Name == rpcParts[1] {
 				// name matched
 				if mtype.PkgPath() != "" || mtype.NumIn() != 3 || mtype.NumOut() != 1 {
-					fmt.Printf("%v is not a valid rpc endpoint method\n", rpcName)
-					os.Exit(1)
+					log.Infof("%v is not a valid rpc endpoint method\n", rpcName)
 					return
 				}
 
@@ -180,8 +235,6 @@ func resolveRPCEntities() (req interface{}, resp interface{}) {
 	}
 
 	// not found
-	fmt.Printf("rpc method %v not found\n", rpcName)
-	os.Exit(1)
-
+	log.Infof("rpc method %v not found\n", rpcName)
 	return
 }

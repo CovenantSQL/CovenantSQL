f3d2259b1218c7f5ddc94a6822c40f25a60d2bdb
Qi Xiao <qi.xiao@covenantsql.io>
2019-01-07 23:38:39 +0800

    Move updatePermission json tag declaration into cql command package

--- types/account_gen_test.go
+++ types/account_gen_test.go
@@ -83,6 +83,43 @@ func BenchmarkAppendMsgMinerInfo(b *testing.B) {
 	}
 }
 
+func TestMarshalHashPermStat(t *testing.T) {
+	v := PermStat{}
+	binary.Read(rand.Reader, binary.BigEndian, &v)
+	bts1, err := v.MarshalHash()
+	if err != nil {
+		t.Fatal(err)
+	}
+	bts2, err := v.MarshalHash()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(bts1, bts2) {
+		t.Fatal("hash not stable")
+	}
+}
+
+func BenchmarkMarshalHashPermStat(b *testing.B) {
+	v := PermStat{}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		v.MarshalHash()
+	}
+}
+
+func BenchmarkAppendMsgPermStat(b *testing.B) {
+	v := PermStat{}
+	bts := make([]byte, 0, v.Msgsize())
+	bts, _ = v.MarshalHash()
+	b.SetBytes(int64(len(bts)))
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bts, _ = v.MarshalHash()
+	}
+}
+
 func TestMarshalHashProviderProfile(t *testing.T) {
 	v := ProviderProfile{}
 	binary.Read(rand.Reader, binary.BigEndian, &v)

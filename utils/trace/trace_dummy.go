d84b295af6d387c0b3eebe927d422c5f957e100d
Qi Xiao <qi.xiao@covenantsql.io>
2019-01-11 13:19:47 +0800

    Add comment to runtime/trace wrapper

--- utils/trace/trace_dummy.go
+++ utils/trace/trace_dummy.go
@@ -23,42 +23,54 @@ import (
 	"io"
 )
 
+// Task mocks runtime/trace.Task.
 type Task struct{}
 
+// End mocks runtime/trace.Task.End.
 func (t *Task) End() {}
 
+// Region mocks runtime/trace.Region.
 type Region struct{}
 
+// End mocks runtime/trace.Region.End.
 func (r *Region) End() {}
 
+// NewTask mocks runtime/trace.NewTask.
 func NewTask(pctx context.Context, taskType string) (ctx context.Context, task *Task) {
 	return ctx, &Task{}
 }
 
+// StartRegion mocks runtime/trace.StartRegion.
 func StartRegion(ctx context.Context, regionType string) {
 	return
 }
 
+// WithRegion mocks runtime/trace.WithRegion.
 func WithRegion(ctx context.Context, regionType string, fn func()) {
 	return
 }
 
+// IsEnabled mocks runtime/trace.IsEnabled.
 func IsEnabled() {
 	return false
 }
 
+// Log mocks runtime/trace.Log.
 func Log(ctx context.Context, category, message string) {
 	return
 }
 
+// Logf mocks runtime/trace.Logf.
 func Logf(ctx context.Context, category, message string, args ...interface{}) {
 	return
 }
 
+// Start mocks runtime/trace.Start.
 func Start(w io.Writer) (err error) {
 	return
 }
 
+// Stop mocks runtime/trace.Stop.
 func Stop() {
 	return
 }
--- utils/trace/trace_go111.go
+++ utils/trace/trace_go111.go
@@ -24,37 +24,48 @@ import (
 	"runtime/trace"
 )
 
+// Task wraps runtime.trace.Task.
 type Task = trace.Task
+
+// Region wraps runtime/trace.Task.
 type Region = trace.Region
 
+// NewTask wraps runtime/trace.NewTask.
 func NewTask(pctx context.Context, taskType string) (ctx context.Context, task *Task) {
 	return trace.NewTask(pctx, taskType)
 }
 
+// StartRegion wraps runtime/trace.StartRegion.
 func StartRegion(ctx context.Context, regionType string) (region *Region) {
 	return trace.StartRegion(ctx, regionType)
 }
 
+// WithRegion wraps runtime/trace.WithRegion.
 func WithRegion(ctx context.Context, regionType string, fn func()) {
 	trace.WithRegion(ctx, regionType, fn)
 }
 
+// IsEnabled wraps runtime/trace.IsEnabled.
 func IsEnabled() bool {
 	return trace.IsEnabled()
 }
 
+// Log wraps runtime/trace.Log.
 func Log(ctx context.Context, category, message string) {
 	trace.Log(ctx, category, message)
 }
 
+// Logf wraps runtime/trace.Logf.
 func Logf(ctx context.Context, category, message string, args ...interface{}) {
 	trace.Logf(ctx, category, message, args...)
 }
 
+// Start wraps runtime/trace.Start.
 func Start(w io.Writer) (err error) {
 	return trace.Start(w)
 }
 
+// Stop wraps runtime/trace.Stop.
 func Stop() {
 	trace.Stop()
 }

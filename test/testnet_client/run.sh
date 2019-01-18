c0f2278a0f261b77e74580296a144e1d46093c7e
laodouya <laodouya@yeah.net>
2019-01-18 12:34:11 +0800

    Increase Particle balance for testnet create db test, add sleep for transaction done.

--- test/testnet_client/run.sh
+++ test/testnet_client/run.sh
@@ -18,13 +18,22 @@ wallet=$(awk '{print $3}' wallet.txt)
 
 #transfer some coin to above address
 ${PROJECT_DIR}/bin/cql -config ${PROJECT_DIR}/conf/testnet/config.yaml -transfer \
-    '{"addr":"'${wallet}'", "amount":"100 Particle"}'
+    '{"addr":"'${wallet}'", "amount":"100000000 Particle"}'
+
+sleep 60
 
 ${PROJECT_DIR}/bin/cql -config conf/config.yaml -create 2 | tee dsn.txt
 
 #get dsn
 dsn=$(cat dsn.txt)
+
+sleep 60
+
 ${PROJECT_DIR}/bin/cql -config conf/config.yaml -get-balance
 ${PROJECT_DIR}/bin/cql -config conf/config.yaml -dsn ${dsn} \
     -command 'create table test_for_new_account(column1 int);'
 
+${PROJECT_DIR}/bin/cql -config conf/config.yaml -dsn ${dsn} \
+    -command 'show tables;' | tee result.log
+
+grep "1 row" result.log

23ca23b763016296244064df4bf2ab44aa52d8ed
Ggicci <ggicci.t@gmail.com>
2019-01-22 17:48:25 +0800

    Upgrade .gitlab-ci.yml

--- alltest.sh
+++ alltest.sh
@@ -19,20 +19,21 @@ test::package() {
 
 main() {
   make clean
-  make use_all_cores
+  make -j6 bp miner observer
 
   # test package by package
   for package in $(go list ./... | grep -v "/vendor/"); do
     test::package "${package}"
   done
 
+  gocovmerge *.cover.out $(find cmd -name "*.cover.out") | grep -F -v '_gen.go' > coverage.txt && rm -f *.cover.out
+  bash <(curl -s https://codecov.io/bash)
+
   # some benchmarks
   go test -bench=^BenchmarkPersistentCaller_Call$ -run ^$ ./rpc/
   bash cleanupDB.sh || true
   go test -bench=^BenchmarkMinerTwo$ -benchtime=5s -run ^$ ./cmd/cql-minerd/
-  go test -bench=^BenchmarkTestnetMiner2$ -benchtime=5s -run ^$ ./cmd/cql-minerd/
-  gocovmerge *.cover.out $(find cmd -name "*.cover.out") | grep -F -v '_gen.go' > coverage.txt && rm -f *.cover.out
-  bash <(curl -s https://codecov.io/bash)
+  bash cleanupDB.sh || true
 }
 
 main "$@"

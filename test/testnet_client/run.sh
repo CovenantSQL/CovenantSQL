2c7b406ecb14c947aa554ca0ee62aa68751e77b9
laodouya <laodouya@yeah.net>
2019-02-25 22:48:42 +0800

    Add param for test old client.

--- test/testnet_client/run.sh
+++ test/testnet_client/run.sh
@@ -5,6 +5,10 @@ set -e
 TEST_WD=$(cd $(dirname $0)/; pwd)
 PROJECT_DIR=$(cd ${TEST_WD}/../../; pwd)
 
+CURRENTBIN=${PROJECT_DIR}/bin
+OLDBIN="/CovenantSQL_bins/v0.3.0"
+BIN=${CURRENTBIN}
+
 echo ${PROJECT_DIR}
 
 # Build
@@ -12,27 +16,32 @@ echo ${PROJECT_DIR}
 # cd ${PROJECT_DIR} && make use_all_cores
 
 cd ${TEST_WD}
-echo -ne "y\n" | ${PROJECT_DIR}/bin/cql-utils -tool confgen -skip-master-key
-${PROJECT_DIR}/bin/cql-utils -tool addrgen -skip-master-key | tee wallet.txt
+
+if [ "old" == "$param" ]; then
+    BIN=${OLDBIN}
+fi
+
+echo -ne "y\n" | ${BIN}/cql-utils -tool confgen -skip-master-key
+${BIN}/cql-utils -tool addrgen -skip-master-key | tee wallet.txt
 
 #get wallet addr
 wallet=$(awk '{print $3}' wallet.txt)
 
 #transfer some coin to above address
-${PROJECT_DIR}/bin/cql -config ${PROJECT_DIR}/conf/testnet/config.yaml -transfer \
+${BIN}/cql -config ${PROJECT_DIR}/conf/testnet/config.yaml -transfer \
     '{"addr":"'${wallet}'", "amount":"100000000 Particle"}' -wait-tx-confirm
 
-${PROJECT_DIR}/bin/cql -get-balance
+${BIN}/cql -get-balance
 
-${PROJECT_DIR}/bin/cql -create 2 -wait-tx-confirm | tee dsn.txt
+${BIN}/cql -create 2 -wait-tx-confirm | tee dsn.txt
 
 #get dsn
 dsn=$(cat dsn.txt)
 
-${PROJECT_DIR}/bin/cql -dsn ${dsn} \
+${BIN}/cql -dsn ${dsn} \
     -command 'create table test_for_new_account(column1 int);'
 
-${PROJECT_DIR}/bin/cql -dsn ${dsn} \
+${BIN}/cql -dsn ${dsn} \
     -command 'show tables;' | tee result.log
 
 grep "1 row" result.log

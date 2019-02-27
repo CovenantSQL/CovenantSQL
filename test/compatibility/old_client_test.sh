b5232b8fb46da4bbffe4fec1c3c1c36dff2b0c6b
laodouya <laodouya@yeah.net>
2019-02-27 20:38:35 +0800

    Fix bp miner not running background.

--- test/compatibility/old_client_test.sh
+++ test/compatibility/old_client_test.sh
@@ -12,33 +12,32 @@ else
     BIN=${PROJECT_DIR}/bin
 fi
 
-cp ${PROJECT_DIR}/test/integration/node_c/config.yaml ~/.cql/
-cp ${PROJECT_DIR}/test/integration/node_c/private.key ~/.cql/
-
+# TODO get pid for kill
 # start current version bp
-nohup ${PROJECT_DIR}/bin/cqld -config ${PROJECT_DIR}/test/integration/node_0/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/bp0.log
-nohup ${PROJECT_DIR}/bin/cqld -config ${PROJECT_DIR}/test/integration/node_1/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/bp1.log
-nohup ${PROJECT_DIR}/bin/cqld -config ${PROJECT_DIR}/test/integration/node_2/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/bp2.log
+nohup ${PROJECT_DIR}/bin/cqld -config ${PROJECT_DIR}/test/integration/node_0/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/bp0.log &
+nohup ${PROJECT_DIR}/bin/cqld -config ${PROJECT_DIR}/test/integration/node_1/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/bp1.log &
+nohup ${PROJECT_DIR}/bin/cqld -config ${PROJECT_DIR}/test/integration/node_2/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/bp2.log &
 
 # wait bp start
 sleep 10
 
 # start current version miner
-nohup ${PROJECT_DIR}/bin/cql-minerd -config ${PROJECT_DIR}/test/integration/node_miner_0/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/miner0.log
-nohup ${PROJECT_DIR}/bin/cql-minerd -config ${PROJECT_DIR}/test/integration/node_miner_0/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/miner1.log
-nohup ${PROJECT_DIR}/bin/cql-minerd -config ${PROJECT_DIR}/test/integration/node_miner_0/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/miner2.log
+nohup ${PROJECT_DIR}/bin/cql-minerd -config ${PROJECT_DIR}/test/integration/node_miner_0/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/miner0.log &
+nohup ${PROJECT_DIR}/bin/cql-minerd -config ${PROJECT_DIR}/test/integration/node_miner_0/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/miner1.log &
+nohup ${PROJECT_DIR}/bin/cql-minerd -config ${PROJECT_DIR}/test/integration/node_miner_0/config.yaml 2>&1 > ${OUTSIDE_BIN_DIR}/miner2.log &
 
-${BIN}/cql -get-balance
 
-${BIN}/cql -create 2 -wait-tx-confirm | tee dsn.txt
+${BIN}/cql -config ${PROJECT_DIR}/test/integration/node_c/config.yaml -get-balance
+
+${BIN}/cql -config ${PROJECT_DIR}/test/integration/node_c/config.yaml -create 2 -wait-tx-confirm | tee dsn.txt
 
 #get dsn
 dsn=$(cat dsn.txt)
 
-${BIN}/cql -dsn ${dsn} \
+${BIN}/cql -config ${PROJECT_DIR}/test/integration/node_c/config.yaml -dsn ${dsn} \
     -command 'create table test_for_new_account(column1 int);'
 
-${BIN}/cql -dsn ${dsn} \
+${BIN}/cql -config ${PROJECT_DIR}/test/integration/node_c/config.yaml -dsn ${dsn} \
     -command 'show tables;' | tee result.log
 
 grep "1 row" result.log

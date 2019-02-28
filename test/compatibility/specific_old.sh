3451b40ba76dec1920779da472254794beaa2edb
laodouya <laodouya@yeah.net>
2019-02-28 18:06:05 +0800

    Use independent config file for compatibility test.

--- test/compatibility/specific_old.sh
+++ test/compatibility/specific_old.sh
@@ -4,12 +4,11 @@ set -e
 
 TEST_WD=$(cd $(dirname $0)/; pwd)
 PROJECT_DIR=$(cd ${TEST_WD}/../../; pwd)
-cd ${TEST_WD}
 
 OLD_BIN_DIR=${CACHE_DIR}/${PREV_VERSION}
 NEW_BIN_DIR=${PROJECT_DIR}/bin
 
-LOGS_DIR=${PIPLINE_CACHE}/logs/old_${test_case}
+LOGS_DIR=${PIPELINE_CACHE}/logs/old_${test_case}
 mkdir -p ${LOGS_DIR}
 
 case $test_case in
@@ -33,26 +32,26 @@ case $test_case in
         ;;
 esac
 
-2>&1
+cd ${TEST_WD}
 # start bp
-nohup ${BPBIN} -config ${PROJECT_DIR}/test/integration/node_0/config.yaml >${LOGS_DIR}/bp0.log 2>&1 &
-nohup ${BPBIN} -config ${PROJECT_DIR}/test/integration/node_1/config.yaml >${LOGS_DIR}/bp1.log 2>&1 &
-nohup ${BPBIN} -config ${PROJECT_DIR}/test/integration/node_2/config.yaml >${LOGS_DIR}/bp2.log 2>&1 &
+nohup ${BPBIN} -config node_0/config.yaml >${LOGS_DIR}/bp0.log 2>&1 &
+nohup ${BPBIN} -config node_1/config.yaml >${LOGS_DIR}/bp1.log 2>&1 &
+nohup ${BPBIN} -config node_2/config.yaml >${LOGS_DIR}/bp2.log 2>&1 &
 
 # wait bp start
 sleep 20
 
 # start miner
-nohup ${MINERBIN} -config ${PROJECT_DIR}/test/integration/node_miner_0/config.yaml >${LOGS_DIR}/miner0.log 2>&1 &
-nohup ${MINERBIN} -config ${PROJECT_DIR}/test/integration/node_miner_1/config.yaml >${LOGS_DIR}/miner1.log 2>&1 &
-nohup ${MINERBIN} -config ${PROJECT_DIR}/test/integration/node_miner_2/config.yaml >${LOGS_DIR}/miner2.log 2>&1 &
+nohup ${MINERBIN} -config node_miner_0/config.yaml >${LOGS_DIR}/miner0.log 2>&1 &
+nohup ${MINERBIN} -config node_miner_1/config.yaml >${LOGS_DIR}/miner1.log 2>&1 &
+nohup ${MINERBIN} -config node_miner_2/config.yaml >${LOGS_DIR}/miner2.log 2>&1 &
 
 # wait miner start
 sleep 20
 
-${CLIENTBIN} -config ${PROJECT_DIR}/test/integration/node_c/config.yaml -get-balance
+${CLIENTBIN} -config node_c/config.yaml -get-balance
 
-${CLIENTBIN} -config ${PROJECT_DIR}/test/integration/node_c/config.yaml -create 2 -wait-tx-confirm | tee dsn.txt
+${CLIENTBIN} -config node_c/config.yaml -create 2 -wait-tx-confirm | tee dsn.txt
 
 #get dsn
 dsn=$(cat dsn.txt)

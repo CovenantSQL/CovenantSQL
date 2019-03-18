14395bc19569ee5b1ef60132c46d1166f4f463b4
Levente Liu <leventeliu@outlook.com>
2019-03-18 19:01:56 +0800

    Add eventual consistency benchmark in GNTE

--- cmd/cql-minerd/benchGNTE.sh
+++ cmd/cql-minerd/benchGNTE.sh
@@ -9,12 +9,14 @@ declare flags=(
 )
 
 fast() {
-    go test        "${flags[@]}" "$pkg"                      | tee -a gnte.log
-    go test        "${flags[@]}" "$pkg" -bench-miner-count=2 | tee -a gnte.log
-    go test -cpu=1 "${flags[@]}" "$pkg" -bench-miner-count=2 | tee -a gnte.log
+    echo "Fast benchmarking with flags: $@"
+    go test        "${flags[@]}" "$pkg" "$@"                      | tee -a gnte.log
+    go test        "${flags[@]}" "$pkg" "$@" -bench-miner-count=2 | tee -a gnte.log
+    go test -cpu=1 "${flags[@]}" "$pkg" "$@" -bench-miner-count=2 | tee -a gnte.log
 }
 
 full() {
+    echo "Full benchmarking with flags: $@"
     local cpus=("" 4 1) counts=(1 2 3 4 8)
     local cpu count caseflags
     for cpu in "${cpus[@]}"; do
@@ -24,7 +26,7 @@ full() {
             caseflags=("-cpu=$cpu" "${flags[@]}")
         fi
         for count in "${counts[@]}"; do
-            go test "${caseflags[@]}" "$pkg" -bench-miner-count=$count | tee -a gnte.log
+            go test "${caseflags[@]}" "$pkg" "$@" -bench-miner-count=$count | tee -a gnte.log
         done
     done
 }
@@ -36,6 +38,7 @@ main() {
         fast
     else
         full
+        full -bench-eventual-consistency
     fi
 }
 

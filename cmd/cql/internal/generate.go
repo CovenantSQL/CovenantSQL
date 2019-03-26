d83b7dc9bcb5985828997f1c36a3d12a248cb077
laodouya <jin.xu@CovenantSQL.io>
2019-03-26 19:01:18 +0800

    Add 'cql generate config' func.

--- cmd/cql/internal/generate.go
+++ cmd/cql/internal/generate.go
@@ -20,10 +20,19 @@ import (
 	"bufio"
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path"
+	"path/filepath"
 	"strings"
 
+	"github.com/CovenantSQL/CovenantSQL/conf/testnet"
 	"github.com/CovenantSQL/CovenantSQL/crypto"
+	"github.com/CovenantSQL/CovenantSQL/crypto/asymmetric"
+	"github.com/CovenantSQL/CovenantSQL/crypto/kms"
+	"github.com/CovenantSQL/CovenantSQL/proto"
+	"github.com/CovenantSQL/CovenantSQL/utils"
+	yaml "gopkg.in/yaml.v2"
 )
 
 // CmdGenerate is cql generate command entity.
@@ -44,7 +53,10 @@ func init() {
 }
 
 func askDeletePath(path string) {
-	if _, err := os.Stat(path); err == nil {
+	if fileinfo, err := os.Stat(path); err == nil {
+		if !fileinfo.IsDir() {
+			path = filepath.Dir(path)
+		}
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Printf("\"%s\" already exists. \nDo you want to delete it? (y or n, press Enter for default n):\n",
 			path)
@@ -78,6 +90,7 @@ func runGenerate(cmd *Command, args []string) {
 
 	switch genType {
 	case "config":
+		configGen()
 	case "wallet":
 		configInit()
 		walletGen()
@@ -93,9 +106,88 @@ func runGenerate(cmd *Command, args []string) {
 }
 
 func configGen() {
-}
+	workingRoot := utils.HomeDirExpand(configFile)
+	if workingRoot == "" {
+		ConsoleLog.Error("config directory is required for generate config")
+		SetExitStatus(1)
+		return
+	}
 
-func privateGen() {
+	if strings.HasSuffix(workingRoot, "config.yaml") {
+		workingRoot = filepath.Dir(workingRoot)
+	}
+
+	askDeletePath(workingRoot)
+
+	privateKeyFileName := "private.key"
+	privateKeyFile := path.Join(workingRoot, privateKeyFileName)
+
+	err := os.Mkdir(workingRoot, 0755)
+	if err != nil {
+		ConsoleLog.WithError(err).Error("unexpected error")
+		SetExitStatus(1)
+		return
+	}
+
+	fmt.Println("Generating key pair...")
+
+	if password == "" {
+		password = readMasterKey(noPassword)
+	}
+
+	privateKey, _, err := asymmetric.GenSecp256k1KeyPair()
+	if err != nil {
+		ConsoleLog.WithError(err).Error("generate key pair failed")
+		SetExitStatus(1)
+		return
+	}
+
+	if err = kms.SavePrivateKey(privateKeyFile, privateKey, []byte(password)); err != nil {
+		ConsoleLog.WithError(err).Error("save generated keypair failed")
+		SetExitStatus(1)
+		return
+	}
+
+	fmt.Printf("Private key file: %s\n", privateKeyFile)
+	fmt.Printf("Public key's hex: %s\n", hex.EncodeToString(privateKey.PubKey().Serialize()))
+	publicKey := privateKey.PubKey()
+
+	fmt.Println("Generated key pair.")
+
+	fmt.Println("Generating nonce...")
+	nonce := nonceGen(publicKey)
+	cliNodeID := proto.NodeID(nonce.Hash.String())
+	fmt.Println("Generated nonce.")
+
+	fmt.Println("Generating config file...")
+	// Load testnet config
+	testnetConfig := testnet.GetTestNetConfig()
+	// Add client config
+	testnetConfig.PrivateKeyFile = privateKeyFileName
+	testnetConfig.ThisNodeID = cliNodeID
+	testnetConfig.KnownNodes = append(testnetConfig.KnownNodes, proto.Node{
+		ID:        cliNodeID,
+		Role:      proto.Client,
+		Addr:      "0.0.0.0:15151",
+		PublicKey: publicKey,
+		Nonce:     nonce.Nonce,
+	})
+
+	// Write config
+	out, err := yaml.Marshal(testnetConfig)
+	if err != nil {
+		ConsoleLog.WithError(err).Error("unexpected error")
+		SetExitStatus(1)
+		return
+	}
+	err = ioutil.WriteFile(path.Join(workingRoot, "config.yaml"), out, 0644)
+	if err != nil {
+		ConsoleLog.WithError(err).Error("unexpected error")
+		SetExitStatus(1)
+		return
+	}
+	fmt.Println("Generated config.")
+
 }
 
 func walletGen() {

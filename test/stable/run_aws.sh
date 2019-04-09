20061c0671c09fecaee21efb93041559ee91a18e
laodouya <jin.xu@CovenantSQL.io>
2019-04-09 20:15:45 +0800

    Stable test: Change running scripts for multi aws instance.

--- test/stable/run_aws.sh
+++ test/stable/run_aws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xeuo pipefail
+set -xeo pipefail
 
 export WORKING_DIR=/home/ubuntu/gopath/src/github.com/CovenantSQL/CovenantSQL
 export RUNNING_DIR=$(cd `dirname $0`; pwd)
@@ -11,4 +11,25 @@ if [[ ! -d "$LOG_DIR" ]]; then
 fi
 
 ./scripts/clean.sh
-./scripts/start.sh
+
+#Monitor
+nohup sar -uqrBWbdv -o ${LOG_DIR}/monitor_uqrBWbdv.log 5 2>&1 > ${LOG_DIR}/monitor_std.log &
+
+#Prepare
+cp -r ${RUNNING_DIR}/docker-compose.yml ${WORKING_DIR}
+
+cd ${WORKING_DIR}
+make docker_clean
+make docker
+
+cd ${RUNNING_DIR}
+if [[ -z "$1" ]]; then
+    ./scripts/start.sh bp
+    ./scripts/start.sh miner0
+    ./scripts/start.sh miner1
+    ./scripts/start.sh miner2
+    ./scripts/start.sh miner3
+    ./scripts/start.sh client
+else
+    ./scripts/start.sh $1
+fi
--- test/stable/scripts/start.sh
+++ test/stable/scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -xeuo pipefail
 
+role=$1
+
 if [ -z "$WORKING_DIR" ]; then
     WORKING_DIR=/home/ubuntu/gopath/src/github.com/CovenantSQL/CovenantSQL
 fi
@@ -11,29 +13,50 @@ if [ -z "$LOG_DIR" ]; then
     LOG_DIR=/data/logs
 fi
 
-#Prepare
-cp -r ${RUNNING_DIR}/docker-compose.yml ${WORKING_DIR}
-cp -r ${RUNNING_DIR}/node_miner_0 /data
-cp -r ${RUNNING_DIR}/node_miner_1 /data
-cp -r ${RUNNING_DIR}/node_miner_2 /data
-cp -r ${RUNNING_DIR}/node_miner_3 /data
-
 cd ${WORKING_DIR}
-make docker_clean
-make docker
-make start
 
-cd ${RUNNING_DIR}
-docker cp covenantsql_bp_1:/app/cql ${RUNNING_DIR}
-sleep 3s
-${RUNNING_DIR}/cql create -config ${RUNNING_DIR}/node_c/config.yaml \
-    -wait-tx-confirm -no-password '{"node":4,"advancepayment": 2000000000}' | tee dsn.txt
-dsn=$(cat dsn.txt)
+case $role in
+    bp)
+        # start bp
+        docker-compose up --no-start covenantsql_bp_0 covenantsql_bp_1 covenantsql_bp_2
+        docker-compose start covenantsql_bp_0 covenantsql_bp_1 covenantsql_bp_2
+        ;;
+    miner0)
+        cp -r ${RUNNING_DIR}/node_miner_0 /data
+        # start miner
+        docker-compose up --no-start covenantsql_miner_0
+        docker-compose start covenantsql_miner_0
+        ;;
+    miner1)
+        cp -r ${RUNNING_DIR}/node_miner_1 /data
+        # start miner
+        docker-compose up --no-start covenantsql_miner_1
+        docker-compose start covenantsql_miner_1
+        ;;
+    miner2)
+        cp -r ${RUNNING_DIR}/node_miner_2 /data
+        # start miner
+        docker-compose up --no-start covenantsql_miner_2
+        docker-compose start covenantsql_miner_2
+        ;;
+    miner3)
+        cp -r ${RUNNING_DIR}/node_miner_3 /data
+        # start miner
+        docker-compose up --no-start covenantsql_miner_3
+        docker-compose start covenantsql_miner_3
+        ;;
+    client)
+        cd ${RUNNING_DIR}
+        docker cp covenantsql_bp_1:/app/cql ${RUNNING_DIR}
+        sleep 3s
+        ${RUNNING_DIR}/cql create -config ${RUNNING_DIR}/node_c/config.yaml \
+            -wait-tx-confirm -no-password '{"node":4,"advancepayment": 2000000000}' | tee dsn.txt
+        dsn=$(cat dsn.txt)
 
-#Start
-go build -o 500million
-nohup ${RUNNING_DIR}/500million -config ${RUNNING_DIR}/node_c/config.yaml -dsn ${dsn} > ${LOG_DIR}/client.log 2>&1 &
+        #Start client
+        go build -o 500million
+        nohup ${RUNNING_DIR}/500million -config ${RUNNING_DIR}/node_c/config.yaml -dsn ${dsn} > ${LOG_DIR}/client.log 2>&1 &
+        ;;
+esac
 
-#Monitor
-nohup sar -uqrBWbdv -o ${LOG_DIR}/monitor_uqrBWbdv.log 5 2>&1 > ${LOG_DIR}/monitor_std.log &
 

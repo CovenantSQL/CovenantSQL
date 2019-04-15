7670972abda0baf77cd90236d9f4a15d035c40f7
laodouya <jin.xu@CovenantSQL.io>
2019-04-15 15:56:55 +0800

    Stable test: get heap add hour and minute in file name.

--- test/stable/scripts/get_heap_now.sh
+++ test/stable/scripts/get_heap_now.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 if [ -z "$LOG_DIR" ]; then
-    LOG_DIR=/data/logs
+    LOG_DIR=/data/logs/mempprof
 fi
 
+mkdir -p ${LOG_DIR}
 
-go tool pprof -png -inuse_objects http://localhost:11112/debug/pprof/heap > ${LOG_DIR}/miner_0_objectinuse.png
-go tool pprof -png http://localhost:11112/debug/pprof/heap > ${LOG_DIR}/miner_0_heapinuse.png
+now=`date +%H_%M`
 
-go tool pprof -png -inuse_objects http://localhost:11113/debug/pprof/heap > ${LOG_DIR}/miner_1_objectinuse.png
-go tool pprof -png http://localhost:11113/debug/pprof/heap > ${LOG_DIR}/miner_1_heapinuse.png
+export GOROOT=/home/ubuntu/go
+export GOBIN=$GOROOT/bin
+export GOPATH=/home/ubuntu/gopath
+export PATH=$GOBIN:$PATH
 
-go tool pprof -png -inuse_objects http://localhost:11114/debug/pprof/heap > ${LOG_DIR}/miner_2_objectinuse.png
-go tool pprof -png http://localhost:11114/debug/pprof/heap > ${LOG_DIR}/miner_2_heapinuse.png
+cd ${LOG_DIR}
 
-go tool pprof -png -inuse_objects http://localhost:11115/debug/pprof/heap > ${LOG_DIR}/miner_3_objectinuse.png
-go tool pprof -png http://localhost:11115/debug/pprof/heap > ${LOG_DIR}/miner_3_heapinuse.png
+which go > ${LOG_DIR}/isgo
 
-go tool pprof -png -inuse_objects http://localhost:6061/debug/pprof/heap > ${LOG_DIR}/client_objectinuse.png
-go tool pprof -png http://localhost:6061/debug/pprof/heap > ${LOG_DIR}/client_heapinuse.png
+go tool pprof -png -inuse_objects http://localhost:11112/debug/pprof/heap > \
+    ${LOG_DIR}/${now}_miner_0_objectinuse.png 2>>${LOG_DIR}/error.log
+go tool pprof -png http://localhost:11112/debug/pprof/heap > \
+    ${LOG_DIR}/${now}_miner_0_heapinuse.png 2>>${LOG_DIR}/error.log
+
+go tool pprof -png -inuse_objects http://localhost:11113/debug/pprof/heap > ${LOG_DIR}/${now}_miner_1_objectinuse.png
+go tool pprof -png http://localhost:11113/debug/pprof/heap > ${LOG_DIR}/${now}_miner_1_heapinuse.png
+
+go tool pprof -png -inuse_objects http://localhost:11114/debug/pprof/heap > ${LOG_DIR}/${now}_miner_2_objectinuse.png
+go tool pprof -png http://localhost:11114/debug/pprof/heap > ${LOG_DIR}/${now}_miner_2_heapinuse.png
+
+go tool pprof -png -inuse_objects http://localhost:11115/debug/pprof/heap > ${LOG_DIR}/${now}_miner_3_objectinuse.png
+go tool pprof -png http://localhost:11115/debug/pprof/heap > ${LOG_DIR}/${now}_miner_3_heapinuse.png
+
+go tool pprof -png -inuse_objects http://localhost:6061/debug/pprof/heap > ${LOG_DIR}/${now}_client_objectinuse.png
+go tool pprof -png http://localhost:6061/debug/pprof/heap > ${LOG_DIR}/${now}_client_heapinuse.png

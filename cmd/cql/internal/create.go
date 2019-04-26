748a8dccc7b7085b36b0976fc93cf9c7ec7d82f3
laodouya <jin.xu@CovenantSQL.io>
2019-04-26 17:34:58 +0800

    Refector cql create command

--- cmd/cql/internal/create.go
+++ cmd/cql/internal/create.go
@@ -18,15 +18,20 @@ package internal
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"math"
+	"strings"
 
 	"github.com/CovenantSQL/CovenantSQL/client"
+	"github.com/CovenantSQL/CovenantSQL/crypto/hash"
+	"github.com/CovenantSQL/CovenantSQL/proto"
 )
+
+var meta client.ResourceMeta
 
 // CmdCreate is cql create command entity.
 var CmdCreate = &Command{
-	UsageLine: "cql create [common params] [-wait-tx-confirm] db_meta_json",
+	UsageLine: "cql create [common params] [-wait-tx-confirm] [db_meta params]",
 	Short:     "create a database",
 	Long: `
 Create creates a CovenantSQL database by database meta info JSON string. The meta info must include
@@ -34,22 +39,24 @@ node count.
 e.g.
     cql create '{"node": 2}'
 
-A complete introduction of db_meta_json fields：
+A complete introduction of db_meta params fields：
 
-    target-miners          []string // List of target miner addresses
-    node                   int      // Target node number
-    space                  int      // Minimum disk space requirement, 0 for none
-    memory                 int      // Minimum memory requirement, 0 for none
-    load-avg-per-cpu       float    // Minimum idle CPU requirement, 0 for none
-    encrypt-key            string   // Encryption key for persistence data
-    eventual-consistency   bool     // Use eventual consistency to sync among miner nodes
-    consistency-level      float    // Consistency level, node*consistency_level is the node number to perform strong consistency
-    isolation-level        int      // Isolation level in a single node
+    target-miners          []string  // List of target miner addresses
+    node                   uint      // Target node number
+    space                  uint      // Minimum disk space requirement, 0 for none
+    memory                 uint      // Minimum memory requirement, 0 for none
+    load-avg-per-cpu       float     // Minimum idle CPU requirement, 0 for none
+    encrypt-key            string    // Encryption key for persistence data
+    eventual-consistency   bool      // Use eventual consistency to sync among miner nodes
+    consistency-level      float     // Consistency level, node*consistency_level is the node number to perform strong consistency
+    isolation-level        int       // Isolation level in a single node
+    gas-price              uint      // customized gas price
+    advance-payment        uint      // customized advance payment
 
 Since CovenantSQL is built on top of blockchains, you may want to wait for the transaction
 confirmation before the creation takes effect.
 e.g.
-    cql create -wait-tx-confirm '{"node": 2}'
+    cql create -wait-tx-confirm -node 2
 `,
 }
 
@@ -58,39 +65,78 @@ func init() {
 
 	addCommonFlags(CmdCreate)
 	addWaitFlag(CmdCreate)
+	addCreateFlags(CmdCreate)
+}
+
+type List struct {
+	Values []string
+}
+
+func (l *List) String() string {
+	return strings.Join(l.Values, ",")
+}
+
+func (l *List) Set(raw string) error {
+	l.Values = strings.Split(raw, ",")
+	return nil
+}
+
+func (l *List) Get() interface{} { return List(*l) }
+
+var targetMiners List
+var node32 uint
+
+func addCreateFlags(cmd *Command) {
+	cmd.Flag.Var(&targetMiners, "target-miners", "List of target miner addresses(seperated by ',')")
+	cmd.Flag.UintVar(&node32, "node", 0, "Target node number")
+	cmd.Flag.Uint64Var(&meta.Space, "space", 0, "Minimum disk space requirement, 0 for none")
+	cmd.Flag.Uint64Var(&meta.Memory, "memory", 0, "Minimum memory requirement, 0 for none")
+	cmd.Flag.Float64Var(&meta.LoadAvgPerCPU, "load-avg-per-cpu", 0, "Minimum idle CPU requirement, 0 for none")
+	cmd.Flag.StringVar(&meta.EncryptionKey, "encrypt-key", "", "Encryption key for persistence data")
+	cmd.Flag.BoolVar(&meta.UseEventualConsistency, "eventual-consistency", false, "Use eventual consistency to sync among miner nodes")
+	cmd.Flag.Float64Var(&meta.ConsistencyLevel, "consistency-level", 0, "Consistency level, node*consistency_level is the node number to perform strong consistency")
+	cmd.Flag.IntVar(&meta.IsolationLevel, "isolation-level", 0, "Isolation level in a single node")
+	cmd.Flag.Uint64Var(&meta.GasPrice, "gas-price", 0, "customized gas price")
+	cmd.Flag.Uint64Var(&meta.AdvancePayment, "advance-payment", 0, "customized advance payment")
 }
 
 func runCreate(cmd *Command, args []string) {
-	if len(args) != 1 {
-		ConsoleLog.Error("Create command need database_meta_info string as params")
+	for _, miner := range targetMiners.Values {
+		targetMiner, err := hash.NewHashFromStr(miner)
+		if err != nil {
+			ConsoleLog.Error("Create target-miners param has invalid node address: ", miner)
+			SetExitStatus(1)
+			return
+		}
+		meta.TargetMiners = append(meta.TargetMiners, proto.AccountAddress(*targetMiner))
+	}
+
+	if node32 > math.MaxUint16 {
+		ConsoleLog.Error("Create node param should not greater than uint16")
+		SetExitStatus(1)
+		return
+	}
+	meta.Node = uint16(node32)
+
+	if meta.Node == 0 {
+		ConsoleLog.Error("Create database failed: request node count must > 1")
 		SetExitStatus(1)
 		help = true
 	}
 
 	configInit(cmd)
-	metaStr := args[0]
+
 	// create database
 	// parse instance requirement
-	var meta client.ResourceMeta
-
-	if err := json.Unmarshal([]byte(metaStr), &meta); err != nil {
-		ConsoleLog.WithField("db", metaStr).Error("create database failed: invalid instance description")
-		SetExitStatus(1)
-		return
-	}
-
-	if meta.Node == 0 {
-		ConsoleLog.WithField("db", metaStr).Error("create database failed: request node count must > 1")
-		SetExitStatus(1)
-		return
-	}
 
 	txHash, dsn, err := client.Create(meta)
 	if err != nil {
-		ConsoleLog.WithError(err).Error("create database failed")
+		ConsoleLog.WithError(err).Error("Create database failed")
 		SetExitStatus(1)
 		return
 	}
+
+	ConsoleLog.Info("Create database requested")
 
 	if waitTxConfirmation {
 		wait(txHash)

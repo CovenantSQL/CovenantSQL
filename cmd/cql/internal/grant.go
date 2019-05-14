95080d5d06fa3ad37280cc0fdae1dceb4a895625
laodouya <jin.xu@CovenantSQL.io>
2019-05-14 09:15:52 +0800

    Refactor cql grant sub-command.

--- cmd/cql/internal/grant.go
+++ cmd/cql/internal/grant.go
@@ -18,25 +18,31 @@ package internal
 
 import (
 	"encoding/json"
+	"strings"
 
 	"github.com/CovenantSQL/CovenantSQL/client"
+	"github.com/CovenantSQL/CovenantSQL/crypto/hash"
 	"github.com/CovenantSQL/CovenantSQL/proto"
 	"github.com/CovenantSQL/CovenantSQL/types"
 )
 
+var (
+	perm string
+)
+
 // CmdGrant is cql grant command entity.
 var CmdGrant = &Command{
-	UsageLine: "cql grant [common params] [-wait-tx-confirm] permission_meta_json",
+	UsageLine: "cql grant [common params] [-wait-tx-confirm] [-to-user wallet] [-to-dsn dsn] [-perm perm_struct]",
 	Short:     "grant a user's permissions on specific sqlchain",
 	Long: `
-Grant grants specific permissions for the target user.
+Grant grants specific permissions for the target user on target dsn.
 e.g.
-    cql grant '{"chain": "your_chain_addr", "user": "user_addr", "perm": "perm_struct"}'
+    cql grant -to-user=43602c17adcc96acf2f68964830bb6ebfbca6834961c0eca0915fcc5270e0b40 -to-dsn="covenantsql://xxxx" -perm perm_struct
 
 Since CovenantSQL is built on top of blockchains, you may want to wait for the transaction
 confirmation before the permission takes effect.
 e.g.
-    cql grant -wait-tx-confirm '{"chain": "your_chain_addr", "user": "user_addr", "perm": "perm_struct"}'
+    cql grant -wait-tx-confirm -to-user=43602c17adcc96acf2f68964830bb6ebfbca6834961c0eca0915fcc5270e0b40 -to-dsn="covenantsql://xxxx" -perm perm_struct
 `,
 }
 
@@ -46,12 +52,9 @@ func init() {
 	addCommonFlags(CmdGrant)
 	addConfigFlag(CmdGrant)
 	addWaitFlag(CmdGrant)
-}
-
-type userPermission struct {
-	TargetChain proto.AccountAddress `json:"chain"`
-	TargetUser  proto.AccountAddress `json:"user"`
-	Perm        json.RawMessage      `json:"perm"`
+	CmdGrant.Flag.StringVar(&toUser, "to-user", "", "Target address of an user account to grant permission.")
+	CmdGrant.Flag.StringVar(&toDSN, "to-dsn", "", "Target database dsn to grant permission.")
+	CmdGrant.Flag.StringVar(&perm, "perm", "", "Permission type struct for grant.")
 }
 
 type userPermPayload struct {
@@ -63,28 +66,45 @@ type userPermPayload struct {
 }
 
 func runGrant(cmd *Command, args []string) {
-	if len(args) != 1 {
-		ConsoleLog.Error("grant command need CovenantSQL perm_meta json string as param")
+	if len(args) > 0 || toUser == "" || toDSN == "" || perm == "" {
+		ConsoleLog.Error("grant command need to-user, to-dsn address and permission struct as param")
 		SetExitStatus(1)
 		help = true
 	}
 
 	commonFlagsInit(cmd)
-	configInit()
-	updatePermission := args[0]
-	// update user's permission on sqlchain
-	var perm userPermission
-	if err := json.Unmarshal([]byte(updatePermission), &perm); err != nil {
-		ConsoleLog.WithError(err).Errorf("update permission failed: invalid permission description")
+
+	if !strings.HasPrefix(toDSN, client.DBScheme) && !strings.HasPrefix(toDSN, client.DBSchemeAlias) {
+		ConsoleLog.Error("grant permission failed: invalid dsn provided, use address start with 'covenantsql://'")
 		SetExitStatus(1)
 		return
 	}
+	toDSN = strings.TrimLeft(toDSN, client.DBScheme+"://")
+	toDSN = strings.TrimLeft(toDSN, client.DBSchemeAlias+"://")
+
+	targetUserHash, err := hash.NewHashFromStr(toUser)
+	if err != nil {
+		ConsoleLog.WithError(err).Error("target user address is not valid")
+		SetExitStatus(1)
+		return
+	}
+	targetUser := proto.AccountAddress(*targetUserHash)
+
+	targetChainHash, err := hash.NewHashFromStr(toDSN)
+	if err != nil {
+		ConsoleLog.WithError(err).Error("target dsn address is not valid")
+		SetExitStatus(1)
+		return
+	}
+	targetChain := proto.AccountAddress(*targetChainHash)
 
 	var permPayload userPermPayload
 
-	if err := json.Unmarshal(perm.Perm, &permPayload); err != nil {
+	if err := json.Unmarshal([]byte(perm), &permPayload); err != nil {
 		// try again using role string representation
-		if err := json.Unmarshal(perm.Perm, &permPayload.Role); err != nil {
+		permPayload.Role.FromString(perm)
+		// invalid struct will set to void
+		if permPayload.Role == types.Void && !strings.EqualFold(perm, "Void") {
 			ConsoleLog.WithError(err).Errorf("update permission failed: invalid permission description")
 			SetExitStatus(1)
 			return
@@ -102,7 +122,9 @@ func runGrant(cmd *Command, args []string) {
 		return
 	}
 
-	txHash, err := client.UpdatePermission(perm.TargetUser, perm.TargetChain, p)
+	configInit()
+
+	txHash, err := client.UpdatePermission(targetUser, targetChain, p)
 	if err != nil {
 		ConsoleLog.WithError(err).Error("update permission failed")
 		SetExitStatus(1)
@@ -117,5 +139,5 @@ func runGrant(cmd *Command, args []string) {
 		}
 	}
 
-	ConsoleLog.Info("succeed in sending transaction to CovenantSQL")
+	ConsoleLog.Info("succeed in grant permission on target database")
 }

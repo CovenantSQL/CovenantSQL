8c8d6308fa4e6f894c58d87a8a5bd6f60981f6f8
744189447 <744189447@qq.com>
2019-05-23 16:02:24 +0800

    Add cql test

---
+++ cmd/cql/internal/create_test.go
@@ -0,0 +1,68 @@
+// +build !testbinary
+
+/*
+ * Copyright 2018-2019 The CovenantSQL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package internal
+
+import (
+	"github.com/CovenantSQL/CovenantSQL/utils"
+	. "github.com/smartystreets/goconvey/convey"
+	"path/filepath"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	FJ := filepath.Join
+	baseDir := utils.GetProjectSrcDir()
+	testWorkingDir := FJ(baseDir, "./test/")
+
+	Convey("test create", t, func(c C) {
+		//targetMiners = List{[]string{"000005aa62048f85da4ae9698ed59c14ec0d48a88a07c15a32265634e7e64ade", "000005f4f22c06f76c43c4f48d5a7ec1309cc94030cbf9ebae814172884ac8b5"}}
+		node32 = 2
+		//meta.Space = 1
+		//meta.Memory = 1000
+		//meta.LoadAvgPerCPU = 1
+		//meta.EncryptionKey = "111"
+		//meta.UseEventualConsistency = true
+		//meta.ConsistencyLevel = 1
+		//meta.IsolationLevel = 1
+		//meta.GasPrice = 1
+		//meta.AdvancePayment = 1
+		waitTxConfirmation = true
+		configFile = FJ(testWorkingDir, "./bench_testnet/node_c/config.yaml")
+		runCreate(CmdCreate, []string{""})
+	})
+
+	//Convey("test create", t, func(c C) {
+	//	//targetMiners = List{[]string{"000005aa62048f85da4ae9698ed59c14ec0d48a88a07c15a32265634e7e64ade", "000005f4f22c06f76c43c4f48d5a7ec1309cc94030cbf9ebae814172884ac8b5"}}
+	//	node32 = 2
+	//	waitTxConfirmation = true
+	//	configFile = FJ(testWorkingDir, "./bench_testnet/node_c/config.yaml")
+	//			jsonStr := `
+	//		{
+	//				"loadavgpercpu": "1",
+	//				"encryptionkey": "123",
+	//				"useeventualconsistency": true,
+	//				"consistencylevel": 1,
+	//				"isolationlevel": 1,
+	//				"gasprice": 100,
+	//				"advancepayment": 100
+	//		}
+	//	`
+	//	runCreate(CmdCreate, []string{jsonStr})
+	//})
+}

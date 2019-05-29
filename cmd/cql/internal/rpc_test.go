c6b147b903b6970821682babfadd36314b366033
744189447 <744189447@qq.com>
2019-05-29 17:45:28 +0800

    Add error tests

--- cmd/cql/internal/rpc_test.go
+++ cmd/cql/internal/rpc_test.go
@@ -42,4 +42,38 @@ func TestRpc(t *testing.T) {
 		SetExitStatus(0)
 		runRPC(CmdRPC, []string{})
 	})
+
+	Convey("rpc", t, func() {
+		// reset
+		commonVarsReset()
+		rpcName = ""
+		rpcEndpoint = ""
+		callBP = false
+		rpcReq = ""
+		client.UnInit()
+
+		rpcName = "MCC.AddTx"
+		rpcEndpoint = "000000fd2c8f68d54d55d97d0ad06c6c0d91104e4e51a7247f3629cc2a0127cf"
+		rpcReq = "{\"DBID\": \"c8328272ba9377acdf1ee8e73b17f2b0f7430c798141080d0282195507eb94e7\"}"
+		configFile = FJ(testWorkingDir, "./bench_testnet/node_c/config.yaml")
+		SetExitStatus(0)
+		runRPC(CmdRPC, []string{})
+	})
+
+	Convey("rpc", t, func() {
+		// reset
+		commonVarsReset()
+		rpcName = ""
+		rpcEndpoint = ""
+		callBP = false
+		rpcReq = ""
+		client.UnInit()
+
+		rpcName = "MCC.AddTx"
+		rpcEndpoint = "000000fd2c8f68d54d55d97d0ad06c6c0d91104e4e51a7247f3629cc2a0127cf"
+		rpcReq = "{\"DBID\" \"c8328272ba9377acdf1ee8e73b17f2b0f7430c798141080d0282195507eb94e7\"}"
+		configFile = FJ(testWorkingDir, "./bench_testnet/node_c/config.yaml")
+		SetExitStatus(0)
+		runRPC(CmdRPC, []string{})
+	})
 }

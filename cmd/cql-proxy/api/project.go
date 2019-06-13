185efd1cccc62fa70d503399d8a780f523971d4e
Qi Xiao <qi.xiao@covenantsql.io>
2019-06-13 21:10:38 +0800

    Fix callback segment typo

--- cmd/cql-proxy/api/project.go
+++ cmd/cql-proxy/api/project.go
@@ -399,7 +399,7 @@ func getProjectOAuthCallback(c *gin.Context) {
 	for _, h := range cfg.Hosts {
 		// project alias happy and host api.covenantsql.io will produce happy.api.covenantsql.io as service host
 		resp = append(resp,
-			fmt.Sprintf("http://%s.%s/auth/callback", p.Alias, strings.TrimLeft(h, ".")))
+			fmt.Sprintf("http://%s.%s/auth/callback/%s", p.Alias, strings.TrimLeft(h, "."), r.Provider))
 	}
 
 	responseWithData(c, http.StatusOK, gin.H{

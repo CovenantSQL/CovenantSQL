84d5dacbcbf76e798494475e4d72171c2a72a202
Qi Xiao <qi.xiao@covenantsql.io>
2019-06-28 07:57:12 +0800

    Add SetPublicMiner tx in compatibility for new bp

--- test/compatibility/specific_old.sh
+++ test/compatibility/specific_old.sh
@@ -49,6 +49,16 @@ nohup ${MINERBIN} -config node_miner_2/config.yaml >${LOGS_DIR}/miner2.log 2>&1 
 # wait miner start
 sleep 20
 
+# use new client and bp_node_c node config to approve miners for public service
+(
+    ${NEW_BIN_DIR}/cql rpc -config node_bp_c/config.yaml -bp -name 'MCC.AddTx' -wait-tx-confirm \
+        -req '{"TxType": 13, "Miner": "ba0ba731c7a76ccef2c1170f42038f7e228dfb474ef0190dfe35d9a37911ed37", "Enabled": 1}'
+    ${NEW_BIN_DIR}/cql rpc -config node_bp_c/config.yaml -bp -name 'MCC.AddTx' -wait-tx-confirm \
+        -req '{"TxType": 13, "Miner": "1a7b0959bbd0d0ec529278a61c0056c277bffe75b2646e1699b46b10a90210be", "Enabled": 1}'
+    ${NEW_BIN_DIR}/cql rpc -config node_bp_c/config.yaml -bp -name 'MCC.AddTx' -wait-tx-confirm \
+        -req '{"TxType": 13, "Miner": "9235bc4130a2ed4e6c35ea189dab35198ebb105640bedb97dd5269cc80863b16", "Enabled": 1}'
+) || [[ "${test_case}" == "bp" ]]
+
 ${CLIENTBIN} wallet -config node_c/config.yaml
 ${CLIENTBIN} create -config node_c/config.yaml -wait-tx-confirm -db-node 2
 
